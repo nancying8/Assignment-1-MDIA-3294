552def22da31e4402f9a93f2bfa4aa3bed5b5e1f
Nancy W <159850766+nancying8@users.noreply.github.com>
2024-10-31 21:10:59 -0700

    Update Footer.jsx

--- Assign1-NancyWu/src/Footer.jsx
+++ Assign1-NancyWu/src/Footer.jsx
@@ -4,8 +4,6 @@ import Instagram from "./assets/instagram.png" // import banner image from asses
 import Youtube from "./assets/tiktok.png" // import banner image from assests to display in this component 
 import Tiktok from "./assets/youtube.png" // import banner image from assests to display in this component 
 
-
-
 // add data for this component "Footer"
 //similar to the component "Header" adding logo, and nav bar
 // apply tailwind css into each section instead using css external 
@@ -17,13 +15,10 @@ function Footer () {
         <div className="flex justify-between items-center">
             <div className="text-center text-5xl text-black ml-6 cursor-pointer "> <a href="#">DogZ</a></div>
             <p>&copy; 2024 DogZ Updates. All rights reserved.</p>
-           
                 <img className="max-w-8" src={Facebook}/>
                 <img className="max-w-8"  src={Instagram}/>
                 <img className="max-w-8"  src={Youtube}/>
                 <img className="max-w-8" src={Tiktok}/>
-
-        
         </div>
     </nav>
 
@@ -31,5 +26,5 @@ function Footer () {
 }
 
 
-// export Footer components to used on App.jsx
+// export Footer component to used on App.jsx
 export default Footer;
